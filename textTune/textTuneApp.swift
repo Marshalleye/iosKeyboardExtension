05074d63883436bae911ddf2a0a759430faeb55f
Nazar <Kapushchaknazar@gmail.com>
2024-03-18 22:40:05 -0400

    Initial Commit

---
+++ textTune/textTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  textTuneApp.swift
+//  textTune
+//
+//  Created by Nazar Kapushchak on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct textTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
